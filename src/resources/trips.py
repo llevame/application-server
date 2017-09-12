34d1d7311b2f0738406d4ad088f5a4e3557b554f
PatricioIribarneCatella <patricio@patricio-Lenovo-V360>
2017-09-11 23:26:40 -0300

    Se agrega la creación de la carpeta logs y se verifica si ya existe al igual que el archivo de log

--- src/resources/trips.py
+++ src/resources/trips.py
@@ -5,17 +5,17 @@ prefix = "/api/v1/trips"
 
 class Trips(Resource):
     def post(self):
-    	logging.info('POST: %s', prefix)
+        logging.info('POST: %s', prefix)
         return 'POST request on ' + prefix
 
 class TripsEstimate(Resource):
     def post(self):
-    	logging.info('POST: %s/estimate', prefix)
+        logging.info('POST: %s/estimate', prefix)
         return 'POST request on ' + prefix + '/estimate'
 
 class TripsIds(Resource):
     def get(self, tripId):
-    	logging.info('GET: %s/%s', prefix, tripId)
+        logging.info('GET: %s/%s', prefix, tripId)
         return 'GET request on ' + prefix + '/' + str(tripId)
 
 
--- src/resources/users.py
+++ src/resources/users.py
@@ -3,12 +3,12 @@ import logging
 
 prefix = "/api/v1/users"
 
-
-
 class Users(Resource):
+
     def get(self):
         logging.info('GET: %s', prefix)
         return 'GET request on ' + prefix
+    
     def post(self):
         logging.info('POST: %s', prefix)
         return 'POST request on ' + prefix
@@ -19,28 +19,35 @@ class UsersValidate(Resource):
         return 'POST request on ' + prefix + '/validate'
 
 class UsersIds(Resource):
+
     def get(self, userId):
         logging.info('GET: %s/%s', prefix, userId)
         return 'GET request on ' + prefix + '/' + str(userId)
+    
     def put(self, userId):
         logging.info('PUT: %s/%s', prefix, userId)
         return 'PUT request on ' + prefix + '/' + str(userId)
+    
     def delete(self, userId):
         logging.info('DELETE: %s/%s', prefix, userId)
         return 'DELETE request on ' + prefix + '/' + str(userId)
 
 class UsersIdsCars(Resource):
+
     def get(self, userId):
         logging.info('GET: %s/%s/cars', prefix, userId)
         return 'GET request on ' + prefix + '/' + str(userId) + '/cars'
+    
     def post(self, userId):
         logging.info('POST: %s/%s/cars', prefix, userId)
         return 'POST request on ' + prefix + '/' +  str(userId) + '/cars'
 
 class UsersIdsCarsIds(Resource):
+
     def get(self, userId, carId):
         logging.info('GET: %s/%s/cars/%s', prefix, userId, carId)
         return 'GET request on ' + prefix + '/' +  str(userId) + '/cars/' + str(carId)
+    
     def put(self, userId, carId):
         logging.info('PUT: %s/%s/cars/%s', prefix, userId, carId)
         return 'PUT request on ' + prefix + '/' +  str(userId) + '/cars/' + str(carId)
@@ -50,9 +57,11 @@ class UsersIdsCarsIds(Resource):
         return 'DELETE request on ' + prefix + '/' +  str(userId) + '/cars/' + str(carId)
 
 class UsersIdsTransactions(Resource):
+
     def get(self, userId):
         logging.info('GET: %s/%s/transactions', prefix, userId)
         return 'GET request on ' + prefix + '/' +  str(userId) + '/transactions'
+    
     def post(self, userId):
         logging.info('POST: %s/%s/transactions', prefix, userId)
         return 'POST request on ' + prefix + '/' +  str(userId) + '/transactions'
