3af4eab75128231bb7e879636510f00fd8c0cbdf
Nicolas Alvarez <dymloz@MacBook-Pro-de-Nicolas.local>
2017-10-01 16:37:59 -0300

    Agrego post al default para agregar un doc a la collection drivers

--- src/resources/default.py
+++ src/resources/default.py
@@ -1,4 +1,6 @@
 from flask_restful import Resource
+from flask import request
+from flask import jsonify
 from pymongo import MongoClient
 from pymongo import errors
 from bson.json_util import dumps
@@ -13,7 +15,7 @@ from managers.dataBaseManager import DataBaseManager
 prefix = "/api/v1"
 
 class Default(Resource):
-    def get(self):	
+	def get(self):	
 		client = MongoClient('mongodb://localhost:27017/')
 		dataBase = client.project # Name of the data base
 		collection = dataBase["collectionName"]
@@ -24,3 +26,12 @@ class Default(Resource):
 
 		logging.info('GET: %s', prefix)
 		return 'Default endpoint on ' + prefix
+		
+	def post(self):
+		client = MongoClient('mongodb://localhost:27017/')
+		dataBase = client.project # Name of the data base
+		driverCollection = dataBase["drivers"]
+		body = request.get_json()
+		driverId = driverCollection.insert_one(body).inserted_id
+		response = {'driverId' : str(driverId), 'name' : body['name'] , 'lastname' : body['lastname'] , 'description' : body['description']}
+		return jsonify(response)
