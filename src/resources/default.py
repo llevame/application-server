bcf241946fab438bb0e953aee2bf2f27befbbde2
Nicolas Alvarez <dymloz@MacBook-Pro-de-Nicolas.local>
2017-10-12 17:44:48 -0300

    Saco base de datos local del endpoint default

--- src/resources/default.py
+++ src/resources/default.py
@@ -16,22 +16,8 @@ prefix = "/api/v1"
 
 class Default(Resource):
 	def get(self):	
-		client = MongoClient('mongodb://localhost:27017/')
-		dataBase = client.project # Name of the data base
-		collection = dataBase["collectionName"]
-		matches = collection.find({"a":1})
-		return dumps(matches)
-
-		#return DataBaseManager().getFrom("asd",{"a":1})
-
-		logging.info('GET: %s', prefix)
 		return 'Default endpoint on ' + prefix
 		
 	def post(self):
-		client = MongoClient('mongodb://localhost:27017/')
-		dataBase = client.project # Name of the data base
-		driverCollection = dataBase["drivers"]
-		body = request.get_json()
-		driverId = driverCollection.insert_one(body).inserted_id
-		response = {'driverId' : str(driverId), 'name' : body['name'] , 'lastname' : body['lastname'] , 'description' : body['description']}
+
 		return jsonify(response)
